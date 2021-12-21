086b49ed4b95c9409f0ab31575332bf244a1a4f5
Moffein <22742925+Moffein@users.noreply.github.com>
2021-12-20 17:56:40 -0800

    Added console command for adding items.

--- EvolutionConfig/EvolutionConfig.cs
+++ EvolutionConfig/EvolutionConfig.cs
@@ -3,13 +3,16 @@ using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using R2API.Utils;
 using RoR2;
+using RoR2.Artifacts;
 using System;
+using UnityEngine;
 
 namespace EvolutionConfig
 {
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.Moffein.EvolutionConfig", "Evolution Config", "1.0.0")]
+    [BepInPlugin("com.Moffein.EvolutionConfig", "Evolution Config", "1.0.1")]
     [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
+    [R2API.Utils.R2APISubmoduleDependency(nameof(CommandHelper))]
     public class EvolutionConfig : BaseUnityPlugin
     {
         public static int whiteCount = 1;
@@ -27,7 +30,6 @@ namespace EvolutionConfig
         public void Awake()
         {
             ReadConfig();
-
             IL.RoR2.Artifacts.MonsterTeamGainsItemsArtifactManager.GrantMonsterTeamItem += (il) =>
             {
                 ILCursor c = new ILCursor(il);
@@ -50,6 +52,57 @@ namespace EvolutionConfig
                     }
                 });
             };
+            R2API.Utils.CommandHelper.AddToConsoleWhenReady();
+        }
+
+        /*private static void RegisterCommands(Action<RoR2.Console> orig, RoR2.Console self)
+        {
+            try
+            {
+                self.concommandCatalog["evolution_grantitems"] = new RoR2.Console.ConCommand
+                {
+                    action = EvolutionConfig_GrantItems,
+                    flags = ConVarFlags.SenderMustBeServer,
+                    helpText = "Add an item to the Evolution pool [ItemName, Count]"
+                };
+            }
+            catch { }
+
+            orig(self);
+        }*/
+
+
+        [ConCommand(commandName = "evolution_additem", flags = ConVarFlags.ExecuteOnServer, helpText = "Add an item to Evolution. [string ItemName, int count]")]
+        public static void EvolutionConfig_GrantItems(ConCommandArgs args)
+        {
+            string itemString = args.TryGetArgString(0);
+            if (args.Count >= 2)
+            {
+                int? itemCount = args.TryGetArgInt(1);
+                if (itemCount.HasValue)
+                {
+                    GrantMonsterTeamItem(itemString, itemCount.Value);
+                    return;
+                }
+            }
+            GrantMonsterTeamItem(itemString);
+        }
+
+        public static void GrantMonsterTeamItem(string itemName, int count = 1)
+        {
+            ItemIndex item = ItemCatalog.FindItemIndex(itemName);
+            if (item != ItemIndex.None)
+            {
+                MonsterTeamGainsItemsArtifactManager.monsterTeamInventory.GiveItem(item, count);
+            }
+            else
+            {
+                Debug.LogError("Item not found. If you have DebugToolkit installed, type list_item to view a list of all internal item names. You must enter the item name as it appears in the list.");
+            }
+        }
+        public static void GrantMonsterTeamItem(ItemIndex itemIndex, int count)
+        {
+            MonsterTeamGainsItemsArtifactManager.monsterTeamInventory.GiveItem(itemIndex, count);
         }
     }
 }
